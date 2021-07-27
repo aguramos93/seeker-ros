f064f90ae79a0d04e9f9657a3acfd2c85a6ec5a1
agu722 <agustin93_rh@hotmail.com>
2021-07-27 11:35:13 +0200

    Publish zoom during a time

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int16MultiArray
-from std_msgs.msg import String
+from seeker_ros.msg import seekerCamera
 import numpy as np
 
 def talker():
 
     pubG = rospy.Publisher('seeker_gimbal', Int16MultiArray, queue_size=1)
-    pubC = rospy.Publisher('seeker_camera', String, queue_size=1)
+    pubC = rospy.Publisher('seeker_camera', seekerCamera, queue_size=1)
     rospy.init_node('talker', anonymous=True)
 
     gimbal = Int16MultiArray()
-    camera = String()
+    camera = seekerCamera()
 
     mode = rospy.get_param('/mode')
     roll = rospy.get_param('/roll')
     pitch = rospy.get_param('/pitch')
     yaw = rospy.get_param('/yaw')
 
-    cmr_cmd = rospy.get_param('/cmr_cmd')
+    cmr_cmd = rospy.get_param('/camera')
+    time = rospy.get_param('/time')
 
     gimbal.data = [mode, roll, pitch, yaw]
-    camera.data = cmr_cmd
+    camera.cmr_cmd = cmr_cmd
+    camera.time = time
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
-        # rospy.loginfo(seeker)
-        pubG.publish(gimbal)
-        pubC.publish(camera)
-        rate.sleep()
+        if camera.cmr_cmd == "zoom_in" or camera.cmr_cmd == "zoom_out":
+            rospy.sleep(camera.time)
+            pubG.publish(gimbal)
+            pubC.publish(camera)
+            rospy.sleep(camera.time)
+
+            camera.cmr_cmd = "stop_zoom"
+            pubC.publish(camera)
+            rate.sleep()
+        else:
+            pubG.publish(gimbal)
+            pubC.publish(camera)
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
